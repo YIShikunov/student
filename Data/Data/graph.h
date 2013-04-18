98c7dccf4f7df7f019cb505cdbe8ca79bbd5bff5
YIShikunov <YIShikunov@gmail.com>
2013-04-18 18:41:59 +0400

    Added template for graph and Dijkstra search algorithm

---
+++ Data/Data/graph.h
@@ -0,0 +1,15 @@
+class Graph
+{
+private:
+	int ** data;
+	int vertexes;
+public:
+	Graph(int ** data, int vertexes)
+	{
+		this -> data = data;
+		this ->vertexes = vertexes;
+	}
+	int * DijkstraSearch()
+	{
+	}
+}
--- Data/Data/main.cpp
+++ Data/Data/main.cpp
@@ -1,2 +1,10 @@
-#include "binTree.h"
+#include "graph.h"
 #include <cstdio>
+
+#define INF -1
+
+void main()
+{
+	int ** d;
+	d[0] = {0,2,5,INF,3};
+}
